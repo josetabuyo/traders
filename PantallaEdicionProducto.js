e821a2121bc1657b587b54e92c6ddd67eaf46b64
unknown <clurgo@NDJ-1208.sds_domain_1.local>
2014-06-06 20:50:47 -0300

    pantalla_producto: empezando con imagenes de producto

--- PantallaEdicionProducto.js
+++ PantallaEdicionProducto.js
@@ -9,6 +9,32 @@ var PantallaEdicionProducto = function(producto){
 		Traders.modificarProducto(producto);
 	});
 	
+    this.imagen_producto = this.ui.find("#imagen_producto");
+    
+    this.imagen_producto.click(function(){
+        var fileInputImagenes = $('<input type="file" />')[0];
+        fileInputImagenes.addEventListener("change", function () {
+            var file = fileInputImagenes.files[0];
+            url = window.URL || window.webkitURL;
+            src = url.createObjectURL(file);
+            var canvas = document.createElement('CANVAS');
+            var ctx = canvas.getContext('2d');
+            var img = new Image;
+            img.crossOrigin = 'Anonymous';
+            img.src = src;
+            img.onload = function () {
+                canvas.height = img.height;
+                canvas.width = img.width;
+                ctx.drawImage(img, 0, 0);
+                var bytes_imagen = canvas.toDataURL('image/jpg');
+                //bytes_imagen = bytes_imagen.replace(/^data:image\/(png|jpg);base64,/, "");        
+                producto.imagen = bytes_imagen;
+                Traders.modificarProducto(producto);
+            };
+        }, false);
+        $(fileInputImagenes).click();
+    });
+    
 	Traders.onNovedades(function(){
 		_this.render();
 	});
@@ -23,4 +49,6 @@ var PantallaEdicionProducto = function(producto){
 
 PantallaEdicionProducto.prototype.render = function(){
 	this.txt_nombre_producto.val(this.producto.nombre);
+    if(this.producto.imagen) this.imagen_producto.attr("src", this.producto.imagen);
+    else this.imagen_producto.attr("src", "Gift-icon-grande.png");
 };
