42d807f4b2479f5a094a3927f7835e0ddf43c569
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2014-06-17 12:08:31 -0300

    celulificacion: pantalla de contactos deslizable

--- PantallaContactos.js
+++ PantallaContactos.js
@@ -1,6 +1,10 @@
 var PantallaContactos = {
 	start:function(){
+		var _this = this;
 		this.ui = $("#pantalla_contactos");	
+		PantallaListaContactos.onSelect(function(){
+			_this.ui.scrollLeft(_this.ui.width());
+		});
 	},
 	render:function(){
 		this.ui.show();
