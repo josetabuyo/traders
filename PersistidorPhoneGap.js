43bf3fa4d9752dc3973f4aef0291f1e8c30dcbe2
josetabuyo <josetabuyo@gmail.com>
2014-07-11 11:18:15 -0300

    storage_phonegap: le agrego punto y coma al select, por favorrr

--- PersistidorPhoneGap.js
+++ PersistidorPhoneGap.js
@@ -62,7 +62,7 @@ var PersistidorPhoneGap = function(opt){
 			
 			var sql = '';
 			sql+='SELECT dato';
-			sql+='	FROM Datos';
+			sql+='	FROM Datos;';
 			//sql+='	WHERE id = "' + id + '";';
 			
 			vx.send({
